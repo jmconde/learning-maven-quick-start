3b7257922c94ea7d57d215035288fe5a9219b8c9
José Conde <josemanuel.conde.campo.next@bbva.com>
2023-04-03 12:44:39 +0200

    Adding dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,14 +2,19 @@ package clinic.programming.training;
 
 import java.util.List;
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words, " ");
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
     public  void greet() {
-	List<String> greets = new ArrayList<>();
-    greets.add("Hello!");
-	for (String greet : greets) {
-          System.out.println("Greeting: " + greet);
-	}
+        List<String> greets = new ArrayList<>();
+        greets.add("Hello!");
+        for (String greet : greets) {
+            System.out.println("Greeting: " + greet);
+        }
     }
     public Application() {
         System.out.println ("Inside Application");
@@ -18,7 +23,9 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
-    app.greet();
+        Application app = new Application();
+        app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word count: " + count);
     }
 }
